edcbfd917125041dcfb10c4a0bbc782b60c99f66
nayan-bansal <nayan.7991@gmail.com>
2020-11-12 04:15:37 +0530

    Added: Wages of the Employee for a Month

--- Employee.js
+++ Employee.js
@@ -7,27 +7,33 @@ if (employeeCheck == IS_ABSENT){
     console.log("Employee is Present");
 }
 
-//UC-2: To Check the Wages of Employee on the basis of Part Time/Full Time
+//UC-4: To Check the Wages of Employee for a Month
 const IS_PART_TIME = 1;
     const IS_FULL_TIME = 2;
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20;
-    
+    const DAYS_PER_MONTH = 20; 
     let employeeHours =  Employee_Hours(employeeCheck);
 
     let employeeWage = employeeHours * WAGE_PER_HOUR;
-    console.log("Employee wage : $" + employeeWage);
+
+    let total_wage = employeeWage*DAYS_PER_MONTH;
+
+    console.log("Total Employee wage : $" + total_wage);
     
     function Employee_Hours(employeeCheck){
     switch (employeeCheck) {
         case IS_PART_TIME:
+            console.log("Employee is Part Time");
             return PART_TIME_HOURS;
             break;
         case IS_FULL_TIME:
+            console.log("Employee is Full Time");
             return FULL_TIME_HOURS;
             break;
         default :
+        console.log("Employee is Absent");
         return 0;
         
     }
