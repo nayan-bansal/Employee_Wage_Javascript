bf0fdf9bd247a4b3e5890af43383b9f16c1f4bc3
nayan-bansal <nayan.7991@gmail.com>
2020-11-11 11:17:49 +0530

    Added: To check Employee is Absent or Present

---
+++ Employee.js
@@ -0,0 +1,9 @@
+//UC_1: To Check EMployee Present or Absent
+let IS_ABSENT = 0;
+let employeeCheck = Math.floor(Math.random()*10)%2;
+if (employeeCheck == IS_ABSENT){
+    console.log("Employee is Absent");
+} else {
+    console.log("Employee is Present");
+}
+
