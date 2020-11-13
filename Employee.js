4b7b2f4a26ba54bf0b53389ee0793fc6f9d27d88
nayan-bansal <nayan.7991@gmail.com>
2020-11-13 10:21:36 +0530

    Added: Storing Employee Wages and Total Wages in Map

--- Employee.js
+++ Employee.js
@@ -16,7 +16,7 @@ const IS_PART_TIME = 1;
     let totalwage = 0;
     let dailywage = 0;
     for(let i = 1; i< 31; i++){
-        if(daycount<20 && hourcount < 160){
+        if(daycount<=20 && hourcount <= 160){
             let employeeCheck = Math.floor(Math.random()*10)%3;
 
             daycount++;
@@ -52,3 +52,104 @@ const IS_PART_TIME = 1;
         
     }
 }
+
+//UC-7A: Total Wages of Employees
+let completewage = 0;
+
+function sum(dailywage){
+    return completewage = completewage + dailywage;
+}
+daily_wage.forEach(sum);
+console.log("Total Workin Days: ",daycount,"\nTotal Working Hours: ",hourcount,"\nTotal Wage: ",completewage);
+
+function total(dailywage){
+    return completewage ;
+}
+
+console.log("(Reduce Method)Total Workin Days: ",daycount,"\nTotal Working Hours: ",hourcount,"\nTotal Wage: ",daily_wage.reduce(total,0));
+
+//UC-7B- Print Day with  Daily Wage
+let daycounter = 0;
+let wagewithday = daily_wage.map(counter);
+console.log(wagewithday);
+
+function counter(dailywage){
+    daycounter++;
+    return "Day- "+daycounter+ " Wage = " + dailywage;
+}
+
+//UC_7C- Show Days Full time wage was earned
+
+let daycounter_fulltime = 0;
+function full_day(dailywage){
+     daycounter_fulltime++ ;
+    
+    if(dailywage ==  FULL_TIME_HOURS * WAGE_PER_HOUR){
+    console.log("Day- ",daycounter_fulltime, "Full time Wage = " ,dailywage);
+
+        return dailywage;
+    }
+}
+console.log("\n");
+
+ wagewithday =  daily_wage.filter(full_day);
+
+ //UC-7D: To display the day of full time employee 
+ let full_count =0,count =0, flag=0;
+ function day(dailywage){
+  
+   
+    full_count++;
+    if(dailywage == 160)
+        count++;
+
+    if(count ==1 && flag ==0){
+    console.log("The first Day with Full time wage is:",full_count);
+        flag = 1;}
+    
+    
+ } 
+ daily_wage.find(day);
+
+
+ //UC-7E: To check if Employee Worked Full time for all days
+ if(count == full_count)
+ console.log("Every Day, the Employee Worked Full Time");
+ else
+ console.log("Every Day, the Employee did not Worked For Full Time");
+
+ //UC-7F: To Check Part Time Employee
+ let part_count =0,p_count=0,p_flag =0;
+ function part_day(dailywage){
+  
+    part_count++;
+    if(dailywage == 80)
+        p_count++;
+
+   if(p_count ==1 && p_flag == 0){
+    console.log("The Array has Part Time Employees");
+p_flag = 1;}
+    
+ } 
+ daily_wage.find(part_day);
+
+ //UC_7G: Count the Total Number of Days Worked
+console.log("The Total No. of Days Worked are: ",full_count);
+
+//UC-8: To store employee wage and day in Map
+
+let employeewagemap  = new Map();
+let totalwagemap = new Map();
+for(let i=1;i<daily_wage.length;i++){
+    employeewagemap.set(i, daily_wage[i]);
+    
+}
+console.log("The Daywise Wages are:\n");
+console.log(employeewagemap);
+
+for(let i=1;i<total_wage.length;i++){
+    totalwagemap.set(i, total_wage[i]);
+    
+}
+console.log("The Daywise Total Wages are:\n");
+console.log(totalwagemap);
