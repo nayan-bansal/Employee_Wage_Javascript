9ac70aea3b5c97e799d7b851bd2ac4834424793a
nayan-bansal <nayan.7991@gmail.com>
2020-11-16 14:35:03 +0530

    Added: Extended Employee Payroll Class to Add Gender and Start Date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -10,11 +10,10 @@ class EmployeePayrollData{
 
     get name() { return this._name;}
     set name(name) {
-        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if(nameRegex.test(name))
+        
         this._name = name;
 
-    else throw 'Name is Incorrect!!' ;
+    
 }
 
     get gender() { return this._gender;}
@@ -29,14 +28,11 @@ class EmployeePayrollData{
 }
 let employee = new EmployeePayrollData(1,"Nayan",50000);
     console.log(employee.toString()); 
-    try{
+    
     employee.name = "Ram";
     employee.startdate = "31-10-2020";
     employee.gender = "M";
     console.log(employee.toString()); 
-    }
-    catch(e){
-        console.error(e);
-    }
+
     let newemployee = new EmployeePayrollData(1,"Lucky",50000,"M","1-1-2020");
     console.log(newemployee.toString()); 
