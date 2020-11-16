dea94f9a7c2ab46070df7eb72cfd348bbfaabb2f
nayan-bansal <nayan.7991@gmail.com>
2020-11-16 20:39:24 +0530

    Added: Ability to checksalary, gender and Date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,12 +1,20 @@
 class EmployeePayrollData{
 
     constructor(...params){
-        this.id = params[0];
+        if(!params[0] > 0) throw "Employee ID Should be Greater than zero"
+        {this.id = params[0];}
+
         this.name = params[1];
+
+        if(params[2] < 0) throw "Salary Should be Greater than zero"
         this.salary = params[2];
+
         this.gender = params[3];
+
+        if(params[4] > new Date()) throw "future Date not allowed"
         this.startdate = params[4];
     }
+
 
     get name() { return this._name;}
     set name(name) {
@@ -15,28 +23,69 @@ class EmployeePayrollData{
         this._name = name;
 
     else throw 'Name is Incorrect!!' ;
-}
+    }
 
     get gender() { return this._gender;}
-    set gender(gender) {this._gender = gender;}
-
-    get startdate() { return this._startdate;}
-    set startdate(startdate) {this._startdate = startdate;}
+    set gender(gender) {
+        let genderRegex = RegExp('[M,F]{1}$');
+        if(genderRegex.test(gender))
+        this._gender = gender;
+        else throw 'Gender is Incorrect';
+    }
 
     toString(){
+        const options = {
+            year: 'numeric', month: 'long', day: 'numeric'
+        };
+        const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-IN", options);
+
         return "\nId: "+this.id+" Name: "+this.name+" Salary: "+this.salary+" Gender: "+this.gender+" Start Date: "+this.startdate;
     }
 }
-let employee = new EmployeePayrollData(1,"Nayan",50000);
+
+try{
+let employee = new EmployeePayrollData(1,"Nayan",50000,"M",new Date("November 30, 2020"));
     console.log(employee.toString()); 
+}
+catch (e){
+    console.log(e);
+} 
+
+try{
+    let employee = new EmployeePayrollData(1,"Nayan",50000,"Y",new Date("November 3, 2020"));
+        console.log(employee.toString()); 
+    }
+    catch (e){
+        console.log(e);
+    }
+
+try{
+     let employee = new EmployeePayrollData(0,"Nayan",50000,"M",new Date("November 3, 2020"));
+        console.log(employee.toString()); 
+            }
+        catch (e){
+            console.log(e);
+        }
+
     try{
-    employee.name = "Ram";
-    employee.startdate = "31-10-2020";
-    employee.gender = "M";
-    console.log(employee.toString()); 
+        let employee = new EmployeePayrollData(1,"Na",50000,"M",new Date("November 3, 2020"));
+                        console.log(employee.toString()); 
+            }
+        catch (e){
+                console.log(e);
+            }
+ 
+            try{
+                let employee = new EmployeePayrollData(1,"Nayan",-50000,"M",new Date("November 3, 2020"));
+                    console.log(employee.toString()); 
+                }
+                catch (e){
+                    console.log(e);
+                }
+try{
+    let employee = new EmployeePayrollData(1,"Nayan",50000,"M",new Date("November 3, 2020"));
+        console.log(employee.toString()); 
     }
-    catch(e){
-        console.error(e);
+    catch (e){
+        console.log(e);
     }
-    let newemployee = new EmployeePayrollData(1,"Lucky",50000,"M","1-1-2020");
-    console.log(newemployee.toString()); 
